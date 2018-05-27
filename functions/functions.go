24a086499d0bd9d923f69f30d5f71e85950551a1
JayWebDevCom <jaiye.s@gmail.com>
2018-05-28 00:39:16 +0100

    Func expressions and variadc/slice

--- functions/functions.go
+++ functions/functions.go
@@ -2,8 +2,7 @@ package main
 
 import "fmt"
 
-// only one parameter type need be declared if the
-// two are the same?
+// only one parameter type need be declared if the two are the same
 func greet(fname, lname string) string {
 	// return fmt.Sprintf("Hello :) %s %s", fname, lname)
 
@@ -29,9 +28,31 @@ func greetPeople(names ...string) (myString string) {
 }
 
 func average(numbers ...float64) (total float64) {
+	fmt.Println("Numbers is:", numbers)
+	fmt.Printf("Type is %T \n", numbers)
 	for _, element := range numbers {
 		total += element
 	}
 	total = total / float64(len(numbers))
 	return
+}
+
+func variadics() float64 {
+	data := []float64{1, 2, 3, 57, 454, 8, 907, 3, 23, 342}
+	// ... provides the slice as variadics i.e. pull each argument out one at a time
+	ave := average(data...)
+	return ave
+}
+
+func anotherAverage(numbers []float64) float64 {
+	var total float64
+	for _, number := range numbers {
+		total += number
+	}
+	return total / float64(len(numbers))
+}
+
+func variadicFunction() float64 {
+	data := []float64{1, 2, 3, 57, 454, 8, 907, 3, 23, 342}
+	return anotherAverage(data)
 }
--- functions/main.go
+++ functions/main.go
@@ -2,11 +2,36 @@ package main
 
 import "fmt"
 
+// returns a func which returns a string
+func makeGreeter() func() string {
+	return func() string {
+		return "Hello make greeter"
+	}
+}
+
 func main() {
+
+	// assigning a function to a variable is a func expression
+	nowGreet := func() {
+		fmt.Println("Hello world")
+	}
+
+	nowGreet()
+	fmt.Printf("nowGreet is a %T \n", nowGreet)
+
+	mGreeter := makeGreeter()
+	fmt.Printf("mGreeter is a %T \n", mGreeter)
+	fmt.Println(mGreeter())
+
 	fmt.Println(greet("Jon", "Doe"))
 	fmt.Println(greetAndReturnVar("Jon", "Doe"))
+
+	fmt.Println()
 	fmt.Println(greetAndReturns("Jon", "Doe"))
 	fmt.Println(greetPeople("Louis", "Peter", "Paul"))
 
+	fmt.Println()
 	fmt.Println(average(3, 33, 333, 3333))
+	fmt.Println(variadics())
+	fmt.Println(variadicFunction())
 }
