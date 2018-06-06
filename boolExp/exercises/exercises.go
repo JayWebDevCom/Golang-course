3e130ec20f04057415193877e1fd911532563364
JayWebDevCom <jaiye.s@gmail.com>
2018-06-06 22:42:55 +0100

    Add exercise

--- boolExp/exercises/exercises.go
+++ boolExp/exercises/exercises.go
@@ -1,6 +1,29 @@
 package exercises
 
-// Exercise1 is exported
+// Exercise1 is exported  - half and isEXEN
 func Exercise1(num int) (float64, bool) {
 	return float64(num) / 2, float64(num)/2 == 0
 }
+
+// Exercise2 is exported - half and isEXEN
+func Exercise2(num int, myCall func(int) bool) (float64, bool) {
+	return float64(num) / 2, myCall(num)
+}
+
+// Exercise3 is exported - greatest in variadic
+func Exercise3(num ...float64) float64 {
+	var ret float64
+	for _, e := range num {
+		if e > ret {
+			ret = e
+		}
+	}
+	return ret
+}
+
+// Exercise4 is exported - length of variadic
+func Exercise4(num ...float64) int {
+	return len(num)
+}
+
+// project euler
--- boolExp/main.go
+++ boolExp/main.go
@@ -24,4 +24,22 @@ func main() {
 
 	fmt.Println(exercises.Exercise1(15))
 
+	fmt.Println(exercises.Exercise2(15, func(num int) bool {
+		return float64(num)/2 == 0
+	}))
+
+	myFunc := func(x int) bool {
+		return float64(x)/2 == 0
+	}
+
+	fmt.Println(exercises.Exercise2(15, myFunc))
+
+	data := []float64{7, 3, 6, 3, 8, 2, 4, 1, 2, 6, 8, 3, 7, 8, 4}
+
+	fmt.Println(exercises.Exercise3(data...))
+
+	fmt.Println(exercises.Exercise4(1))
+	fmt.Println(exercises.Exercise4(1, 2))
+	fmt.Println(exercises.Exercise4(data...))
+
 }
